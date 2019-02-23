225b540c677737c8578ce06055ac12f79c9b5fcf
Ewa Sroczyk <es@nutopi.com>
2019-02-23 17:22:47 +0100

    Protocols for multi-channel pipette volumes testing created.

---
+++ volumesTest/MultiVolTestSameTip.py
@@ -0,0 +1,50 @@
+from opentrons import labware, instruments, robot
+
+# creates custom labware if currently not created
+custom_tube = "tray"
+if custom_tube not in labware.list():
+    tray = labware.create("tray",
+                          grid=(1, 1),
+                          spacing=(0, 0),
+                          diameter=60,
+                          depth=15)
+
+tray = labware.load("tray", slot='5')
+trough_rack = labware.load('trough-12row', slot='11')
+tips300ul_rack1 = labware.load('opentrons-tiprack-300ul', slot='10')
+
+multi = instruments.P300_Multi(mount='left',
+                               tip_racks=[tips300ul_rack1],
+                               aspirate_flow_rate=100,
+                               dispense_flow_rate=100)
+multi.pick_up_tip()
+
+def multi_dispensing(disp_vol, times):
+    for i in range(times):
+        curr_vol = multi.current_volume
+        if curr_vol < disp_vol:
+            multi.dispense(curr_vol, trough_rack(6))
+            multi.aspirate(disp_vol, trough_rack(6))
+        multi.dispense(disp_vol, tray)
+        robot.pause()
+        print(disp_vol, ' ul dispensed ', i+1, ' time')
+    multi.drop_tip()
+    robot.home()
+
+
+def multi_transfering(disp_vol, times):
+    for i in range(times):
+        multi.aspirate(disp_vol, trough_rack(6))
+        multi.dispense(disp_vol, tray)
+        multi.blow_out(tray)
+        robot.pause()
+        print(disp_vol, ' ul dispensed ', i+1, ' time')
+    multi.drop_tip()
+    robot.home()
+
+# *************************************** #
+# each channel 4 times
+# multi range: 30ul-300ul
+times1 = 32
+disp_vol1 = 135
+multi_transfering(disp_vol1, times1)
