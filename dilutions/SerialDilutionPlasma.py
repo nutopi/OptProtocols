32ae75b91376a95c5cf8ecf9765243fab3791259
Ewa Sroczyk <es@nutopi.com>
2019-06-21 14:49:12 +0200

    Serial dilution protocol initialized. Labware defined.

--- dilutions/SerialDilutionPlasma.py
+++ dilutions/SerialDilutionPlasma.py
@@ -1,49 +1,50 @@
 from opentrons import labware, instruments, robot
-from opentrons.data_storage import database
 
-
-def volume_test(disp_vol, times, tube_on_weight):
+def serial_dil(diff, final_vol, tube_amount):
     labware_list = labware.list()
 
-    if labware_list.__contains__(tube_on_weight):
-        database.delete_container(tube_on_weight)
+# creates custom labware if currently not created
+    if tube_2ml_rack_35 not in labware.list():
+        tube_2ml_rack_35 = labware.create("tube_2ml_rack_35",
+                                          grid=(7,5),
+                                          spacing=(16.25, 14.56),
+                                          diameter=10,
+                                          depth=30)
 
-    labware.create(tube_on_weight,
-                   grid=(1, 1),
-                   spacing=(0, 0),
-                   diameter=10,
-                   depth=30,
-                   volume=200)
+    2ml_tubes1 = labware.load('tube_2ml_rack_35', slot='1)
+    2ml_tubes2 = labware.load('tube_2ml_rack_35', slot='2)
+    2ml_tubes3 = labware.load('tube_2ml_rack_35', slot='3)
+    tip_rack_small = labware.load('opentrons-tiprack-300ul', slot='4)
+    #TODO: check the big tip rack
+    tip_rack_big = labware.load('tiprack-1000ul', slot='5')
+    plasma_falcon = labware.load('opentrons-tuberack-15_50ml', slot='6')
 
-    weight_tube = labware.load(tube_on_weight, slot='5')
-    trough_rack = labware.load('trough-12row', slot='11')
-    tips300ul_rack1 = labware.load('tiprack-200ul', slot='10')
-
-    single = instruments.P50_Single(mount='right',
-                                    tip_racks=[tips300ul_rack1],
+    single50 = instruments.P50_Single(mount='right',
+                                    tip_racks=tip_rack_small,
                                     aspirate_flow_rate=100,
                                     dispense_flow_rate=100)
 
-    single.pick_up_tip()
+    single1000 = instruments.P1000_Single(mount='left',
+                                      tip_racks=tip_rack_big,
+                                      aspirate_flow_rate=100,
+                                      dispense_flow_rate=100)
 
-    for i in range(times):
-        curr_vol = single.current_volume
-        if curr_vol < disp_vol:
-            single.dispense(curr_vol, trough_rack(0))
-            single.aspirate(single.max_volume, trough_rack(0))
-            single.move_to(weight_tube(0))
-            robot.pause()
-        single.dispense(disp_vol, weight_tube(0))
-        robot.pause()
-        print('Read weight, then press Resume button, lol xD')
-
-    single.dispense(single.current_volume, trough_rack(0))
-    robot.home('z')
+    #    for i in range(times):
+    #     curr_vol = single.current_volume
+    #     if curr_vol < disp_vol:
+    #         single.dispense(curr_vol, trough_rack(0))
+    #         single.aspirate(single.max_volume, trough_rack(0))
+    #         single.move_to(weight_tube(0))
+    #         robot.pause()
+    #     single.dispense(disp_vol, weight_tube(0))
+    #     robot.pause()
+    #     print('Read weight, then press Resume button, lol xD')
+    #
+    # single.dispense(single.current_volume, trough_rack(0))
+    # robot.home('z')
 
 
 # ****************************************************************************************#
 
-disp_vol1 = 10
-times1 = 10
-tube_type = 'tube_2ml_weight'
-volume_test(disp_vol1, times1, tube_type)
+#wywolanie funkcji
+tube_amount = round(final_vol/diff)
