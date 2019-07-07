7d38cf20829ec86be5af06976d4cdf52e709df3e
Ewa Sroczyk <es@nutopi.com>
2019-07-07 16:28:28 +0200

    Dilution Preparing protocol refactored. Two new methods created and used. Tubes iterating and round the difference value calculated in a correct way.

--- dilutions/SerialDilutionPlasma.py
+++ dilutions/SerialDilutionPlasma.py
@@ -1,11 +1,10 @@
 from opentrons import labware, instruments, robot
+import math
 
 
 def serial_dil(begin_vol, final_vol, tube_amount):
-    #TODO make a condition to check
-    #begin_vol > final_vol * tube_amount
-
-    labware_list = labware.list()
+    # TODO make a condition to check
+    # begin_vol > final_vol * needed_tube
 
     # creates custom labware if currently not created
     tube_2ml_rack_35 = "tube_2ml_rack_35"
@@ -37,37 +36,15 @@ def serial_dil(begin_vol, final_vol, tube_amount):
                                           dispense_flow_rate=100)
 
     current_vol = begin_vol
-    diff = round(final_vol / tube_amount)
+    diff = math.floor(final_vol / (tube_amount - 1))
+
+    # calculating of real tube amount by rounding up
+    needed_tube = math.ceil(final_vol / diff)
+    # TODO printout to opentrons app
+    print(needed_tube, 'tubes is needed.')
 
     # dispensing plasma A from 0 ul to final volume
-    single50.pick_up_tip()
-    single1000.pick_up_tip()
-
-    for i in range(tube_amount):
-        current_tube = i + 1
-        if current_vol >= 5000:
-            height = 1.8 * current_vol - 109
-            if i * diff < 100:
-                single50.transfer(i * diff, plasma_falcon('A3').top(height), tubes)
-                current_vol = current_vol - i * diff
-                # print('current volume of plasma A: ', current_vol)
-                print(i * diff, ' ul of plasma A added to tube ', current_tube)
-            else:
-                single1000.transfer(i * diff, plasma_falcon('A3').top(height), tubes)
-                current_vol = current_vol - i * diff
-                # print('current volume of plasma A: ', current_vol)
-                print(i * diff, ' ul of plasma A added to tube ', current_tube)
-        else:
-            if i * diff < 100:
-                single50.transfer(i * diff, plasma_falcon('A3').bottom(3), tubes)
-                current_vol = current_vol - i * diff
-                # print('current volume of plasma A: ', current_vol)
-                print(i * diff, ' ul of plasma A added to tube ', current_tube)
-            else:
-                single1000.transfer(i * diff, plasma_falcon('A3').bottom(3), tubes)
-                current_vol = current_vol - i * diff
-                # print('current volume of plasma A: ', current_vol)
-                print(i * diff, ' ul of plasma A added to tube ', current_tube)
+    dispense_plasma(True, needed_tube, diff, final_vol, single50, single1000, plasma_falcon, tubes, current_vol)
 
     current_vol = begin_vol
 
@@ -75,37 +52,51 @@ def serial_dil(begin_vol, final_vol, tube_amount):
     tubes = [tubes1, tubes2, tubes3, tubes4]
 
     # dispensing plasma B from final volume to 0 ul
+    dispense_plasma(False, needed_tube, diff, final_vol, single50, single1000, plasma_falcon, tubes, current_vol)
+
+    print(needed_tube, 'dilutions prepared.')
+
+
+def dispense_plasma(is_increase, needed_tube, diff, final_vol, single50, single1000, plasma_falcon, tubes, current_vol):
     single50.pick_up_tip()
     single1000.pick_up_tip()
 
-    for i in range(tube_amount):
+    for i in range(needed_tube):
         current_tube = i + 1
+        transfer_vol = calc_transfer_vol(is_increase, final_vol, diff, i)
         if current_vol >= 5000:
-            height = 1.8 * current_vol - 109
-            if final_vol - i * diff < 100:
-                single50.transfer(final_vol - i * diff, plasma_falcon('A4').top(height), tubes)
-                current_vol = current_vol - (final_vol - i * diff)
-                # print('current volume of plasma B: ', current_vol)
-                print(final_vol - i * diff, ' ul of plasma B added to tube ', current_tube)
+            height = 0.0018 * current_vol - 109
+            if transfer_vol < 100:
+                single50.transfer(transfer_vol, plasma_falcon('A3').top(height), tubes, new_tip='never')
+                current_vol = current_vol - transfer_vol
+                # print('current volume of plasma A: ', current_vol)
+                print(transfer_vol, ' ul of plasma A added to tube ', current_tube)
             else:
-                single1000.transfer(final_vol - i * diff, plasma_falcon('A4').top(height), tubes)
-                current_vol = current_vol - (final_vol - i * diff)
-                # print('current volume of plasma B: ', current_vol)
-                print(final_vol - i * diff, ' ul of plasma B added to tube ', current_tube)
-
+                single1000.transfer(transfer_vol, plasma_falcon('A3').top(height), tubes, new_tip='never')
+                current_vol = current_vol - transfer_vol
+                # print('current volume of plasma A: ', current_vol)
+                print(transfer_vol, ' ul of plasma A added to tube ', current_tube)
         else:
-            if final_vol - i * diff < 100:
-                single50.transfer(final_vol - i * diff, plasma_falcon('A4').bottom(3), tubes)
-                current_vol = current_vol - (final_vol - i * diff)
-                # print('current volume of plasma B: ', current_vol)
-                print(final_vol - i * diff, ' ul of plasma B added to tube ', current_tube)
+            if transfer_vol < 100:
+                single50.transfer(transfer_vol, plasma_falcon('A3').bottom(3), tubes, new_tip='never')
+                current_vol = current_vol - transfer_vol
+                # print('current volume of plasma A: ', current_vol)
+                print(transfer_vol, ' ul of plasma A added to tube ', current_tube)
             else:
-                single1000.transfer(final_vol - i * diff, plasma_falcon('A4').bottom(3), tubes)
-                current_vol = current_vol - (final_vol - i * diff)
-                # print('current volume of plasma B: ', current_vol)
-                print(final_vol - i * diff, ' ul of plasma B added to tube ', current_tube)
+                single1000.transfer(transfer_vol, plasma_falcon('A3').bottom(3), tubes, new_tip='never')
+                current_vol = current_vol - transfer_vol
+                # print('current volume of plasma A: ', current_vol)
+                print(transfer_vol, ' ul of plasma A added to tube ', current_tube)
 
-    print(tube_amount, 'dilutions prepared.')
+    single50.drop_tip()
+    single1000.drop_tip()
+
+
+def calc_transfer_vol(is_increase, final_vol, diff, iteration):
+    if is_increase:
+        return iteration * diff
+    else:
+        return final_vol - iteration * diff
 
 
 # ****************************************************************************************#
@@ -114,5 +105,5 @@ def serial_dil(begin_vol, final_vol, tube_amount):
 begin_vol1 = 30000
 final_vol1 = 600
 tube_amount1 = 35
-#diff = round(final_vol1 / tube_amount1)
+# diff = round(final_vol1 / tube_amount1)
 serial_dil(begin_vol1, final_vol1, tube_amount1)
