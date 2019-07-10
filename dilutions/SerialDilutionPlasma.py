18025cf0ed5188ea859e66434c896ec87f199c34
Ewa Sroczyk <es@nutopi.com>
2019-07-10 14:45:23 +0200

    Needed_tube calculating removed. Rounding difference value in calc_transfer_vol added. Message to user in the app added if begin volume is too little.

--- dilutions/SerialDilutionPlasma.py
+++ dilutions/SerialDilutionPlasma.py
@@ -1,7 +1,5 @@
 from opentrons import labware, instruments, robot
-import math
 
-robot.clear_commands()
 
 def serial_dil(begin_vol, final_vol, tube_amount):
     # creates custom labware if currently not created
@@ -32,85 +30,89 @@ def serial_dil(begin_vol, final_vol, tube_amount):
                                           tip_racks=[tip_rack_big],
                                           aspirate_flow_rate=100,
                                           dispense_flow_rate=100)
-
+    # difference value calculating
     current_vol = begin_vol
-    diff = math.floor(final_vol / (tube_amount - 1))
+    diff = round(final_vol / (tube_amount - 1), 2)
 
-    # calculating of real tube amount by rounding up
-    needed_tube = math.ceil(final_vol / diff)
+    # text = 'Amount of needed tubes is {needed_tube}'.format(
+    #     needed_tube=needed_tube
+    # )
+    # robot.comment(text)
 
-    #robot.comment(needed_tube, 'tubes is needed.')
-    print(robot.comment('tubes is needed.'))
-
-    # for c in robot.commands():
-    #     print(c)
-
-    if begin_vol < calc_min_begin_vol(needed_tube, diff):
-        print('Begin volume is too little. Pour more liquid into the falcon.')
+    if begin_vol < calc_min_begin_vol(tube_amount, diff):
+        robot.comment('Begin volume is too little. Pour more liquid into the falcon.')
         return
 
     # dispensing plasma A from 0 ul to final volume
-    dispense_plasma(True, needed_tube, diff, final_vol, single50, single1000, plasma_falcon('A3'), tubes, current_vol)
+    dispense_plasma(True, tube_amount, diff, final_vol, single50, single1000, plasma_falcon('A3'), tubes, current_vol)
 
     current_vol = begin_vol
 
     # tubes iteration from scratch
     tubes = [tubes1, tubes2, tubes3, tubes4]
 
+    # tubes in racks iteration from scratch
+    global current_rack
+    current_rack = 0
+
     # dispensing plasma B from final volume to 0 ul
-    dispense_plasma(False, needed_tube, diff, final_vol, single50, single1000, plasma_falcon('A4'), tubes, current_vol)
+    dispense_plasma(False, tube_amount, diff, final_vol, single50, single1000, plasma_falcon('A4'), tubes, current_vol)
 
-    print(needed_tube, 'dilutions prepared.')
+    print(tube_amount, 'dilutions prepared.')
 
 
-def dispense_plasma(is_increase, needed_tube, diff, final_vol, single50, single1000, source, tubes, current_vol):
+def dispense_plasma(is_increase, tube_amount, diff, final_vol, single50, single1000, source, tubes, current_vol):
     single50.pick_up_tip()
     single1000.pick_up_tip()
 
     counter_single50 = 0
     counter_single1000 = 0
 
-    for i in range(needed_tube):
+    for i in range(tube_amount):
         current_tube = i + 1
         transfer_vol = calc_transfer_vol(is_increase, final_vol, diff, i)
         if current_vol >= 5000:
             height = 0.0018 * current_vol - 118
             if transfer_vol < 100:
                 check_if_tip_replace(single50, counter_single50)
-                single50.transfer(transfer_vol, source.top(height), which_well_dest(tubes, i).top(-15), new_tip='never', blow_out=True)
+                single50.transfer(transfer_vol, source.top(height), which_well_dest(tubes, i).top(-15), new_tip='never',
+                                  blow_out=True)
+                single50.blow_out()
                 single50.blow_out()
                 single50.blow_out()
                 current_vol = current_vol - transfer_vol
-                # print('current volume of plasma A: ', current_vol)
                 print(transfer_vol, ' ul of plasma added to tube ', current_tube)
                 counter_single50 = counter_single50 + 1
             else:
                 check_if_tip_replace(single1000, counter_single1000)
-                single1000.transfer(transfer_vol, source.top(height), which_well_dest(tubes, i).top(-15), new_tip='never', blow_out=True)
+                single1000.transfer(transfer_vol, source.top(height), which_well_dest(tubes, i).top(-15),
+                                    new_tip='never', blow_out=True)
+                single1000.blow_out()
                 single1000.blow_out()
                 single1000.blow_out()
                 current_vol = current_vol - transfer_vol
-                # print('current volume of plasma A: ', current_vol)
                 print(transfer_vol, ' ul of plasma added to tube ', current_tube)
                 counter_single1000 = counter_single1000 + 1
         else:
             if transfer_vol < 100:
                 check_if_tip_replace(single50, counter_single50)
-                single50.transfer(transfer_vol, source.bottom(3), which_well_dest(tubes, i).top(-15), new_tip='never', blow_out=True)
+                single50.transfer(transfer_vol, source.bottom(3), which_well_dest(tubes, i).top(-15), new_tip='never',
+                                  blow_out=True)
+                single50.blow_out()
                 single50.blow_out()
                 single50.blow_out()
                 current_vol = current_vol - transfer_vol
-                # print('current volume of plasma A: ', current_vol)
                 print(transfer_vol, ' ul of plasma added to tube ', current_tube)
                 counter_single50 = counter_single50 + 1
 
             else:
                 check_if_tip_replace(single1000, counter_single1000)
-                single1000.transfer(transfer_vol, source.bottom(3), which_well_dest(tubes, i).top(-15), new_tip='never', blow_out=True)
+                single1000.transfer(transfer_vol, source.bottom(3), which_well_dest(tubes, i).top(-15), new_tip='never',
+                                    blow_out=True)
+                single1000.blow_out()
                 single1000.blow_out()
                 single1000.blow_out()
                 current_vol = current_vol - transfer_vol
-                # print('current volume of plasma A: ', current_vol)
                 print(transfer_vol, ' ul of plasma added to tube ', current_tube)
                 counter_single1000 = counter_single1000 + 1
 
@@ -120,14 +122,14 @@ def dispense_plasma(is_increase, needed_tube, diff, final_vol, single50, single1
 
 def calc_transfer_vol(is_increase, final_vol, diff, iteration):
     if is_increase:
-        return iteration * diff
+        return round(iteration * diff)
     else:
-        return final_vol - iteration * diff
+        return round(final_vol - iteration * diff)
 
 
-def calc_min_begin_vol(needed_tube, diff):
+def calc_min_begin_vol(tube_amount, diff):
     calculated_min = 0
-    for i in range(needed_tube):
+    for i in range(tube_amount):
         calculated_min = calculated_min + i * diff
     return calculated_min
 
@@ -139,22 +141,23 @@ def check_if_tip_replace(pipette, counter_pipette):
         pipette.pick_up_tip()
 
 
+current_rack = 0
+
+
 # destination place iterating
 def which_well_dest(tubes, iteration):
+    global current_rack
     tubes_amount_in_rack = 35
-    if iteration < tubes_amount_in_rack:
-        return tubes[0][iteration]
-    elif (iteration >= tubes_amount_in_rack) and (iteration < tubes_amount_in_rack * 2):
-        return tubes[1][iteration - tubes_amount_in_rack]
-    elif (iteration >= tubes_amount_in_rack * 2) and (iteration < tubes_amount_in_rack * 3):
-        return tubes[2][iteration - (tubes_amount_in_rack * 2)]
-    elif (iteration >= tubes_amount_in_rack * 3) and (iteration < tubes_amount_in_rack * 4):
-        return tubes[3][iteration - (tubes_amount_in_rack * 3)]
+    if iteration % tubes_amount_in_rack == 0 and iteration != 0:
+        current_rack = current_rack + 1
+
+    return tubes[current_rack][iteration - (tubes_amount_in_rack * current_rack)]
+
 
 # ****************************************************************************************#
 
 # invoke method
-begin_vol1 = 20000
+begin_vol1 = 40000
 final_vol1 = 600
-tube_amount1 = 10
+tube_amount1 = 104
 serial_dil(begin_vol1, final_vol1, tube_amount1)
