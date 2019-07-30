fc7d7e3401e92778bc51168c3f0ae37a19f41605
Ewa Sroczyk <es@nutopi.com>
2019-07-30 12:19:39 +0200

    Protocol for dividing proviously prepared dilutions. TODO - correct source rack counting and correct wells counting (A1, B1, C1... instead of A1, A2, A3...). Probably a problem with global variable. Changing pipette tips must me added when this protocol will be working correctly.

---
+++ dilutions/AliquotDilutions.py
@@ -0,0 +1,101 @@
+from opentrons import labware, instruments, robot
+
+current_rack = 0
+first_rack = 0
+last_rack = 2
+
+
+def aliquot_dil():
+    # creates custom labware if currently not created
+    tube_2ml_rack_35 = "tube_2ml_rack_35"
+    if tube_2ml_rack_35 not in labware.list():
+        tube_2ml_rack_35 = labware.create("tube_2ml_rack_35",
+                                          grid=(7, 5),
+                                          spacing=(16.25, 14.56),
+                                          diameter=10,
+                                          depth=30)
+
+    # labware placement
+    source_tubes_a = labware.load('tube_2ml_rack_35', slot='1')
+    source_tubes_b = labware.load('tube_2ml_rack_35', slot='2')
+    source_tubes_c = labware.load('tube_2ml_rack_35', slot='3')
+    source_tubes = [source_tubes_a, source_tubes_b, source_tubes_c]
+
+    dest_tubes_reverse_a = labware.load('tube_2ml_rack_35', slot='4')
+    dest_tubes_reverse_b = labware.load('tube_2ml_rack_35', slot='5')
+    dest_tubes_reverse_c = labware.load('tube_2ml_rack_35', slot='6')
+    dest_tubes_reverse = [dest_tubes_reverse_a, dest_tubes_reverse_b, dest_tubes_reverse_c]
+
+    dest_tubes_normal_a = labware.load('tube_2ml_rack_35', slot='7')
+    dest_tubes_normal_b = labware.load('tube_2ml_rack_35', slot='8')
+    dest_tubes_normal_c = labware.load('tube_2ml_rack_35', slot='9')
+    dest_tubes_normal = [dest_tubes_normal_a, dest_tubes_normal_b, dest_tubes_normal_c]
+
+    tip_rack_big = labware.load('tiprack-1000ul', slot='11')
+
+    # pipette definition
+    single1000 = instruments.P1000_Single(mount='left',
+                                          tip_racks=[tip_rack_big],
+                                          aspirate_flow_rate=1000,
+                                          dispense_flow_rate=1500)
+    global current_rack
+    # TODO proper wells counting in source racks
+    single1000.pick_up_tip(tip_rack_big, 3, 0)
+    for i in range(105):
+        transfer(True, single1000, 200, i, source_tubes, dest_tubes_reverse, dest_tubes_normal)
+        transfer(False, single1000, 200, i, source_tubes, dest_tubes_reverse, dest_tubes_normal)
+
+
+# source tubes iterating
+def source(iteration, source_tubes):
+    global current_rack
+    if iteration % 35 == 0 and iteration != 0:
+        current_rack = current_rack + 1
+    print('\nSOURCE:', source_tubes[current_rack][iteration - (35 * current_rack)], '\tsource rack:', current_rack)
+    return source_tubes[current_rack][iteration - (35 * current_rack)]
+
+
+# destination place iterating
+def destination(is_increase, dest_tubes_reverse, dest_tubes_normal, iteration):
+    global first_rack
+    global last_rack
+    if is_increase:
+        if iteration % 35 == 0 and iteration != 0:
+            first_rack = first_rack + 1
+        print('DEST NORMAL:', dest_tubes_normal[first_rack][iteration - (35 * first_rack)], '\tdest rack:', first_rack)
+        return dest_tubes_normal[first_rack][iteration - (35 * first_rack)]
+    else:
+        if (105 - iteration) % 35 == 0 and iteration != 0:
+            last_rack = last_rack - 1
+        print('DEST REVERSE:', dest_tubes_reverse[last_rack][105 - last_rack * 35 - iteration - 1], '\tdest rack:',
+              last_rack, '\n***')
+        return dest_tubes_reverse[last_rack][105 - last_rack * 35 - iteration - 1]
+
+
+def transfer(is_increase, pipette, transfer_vol, iteration, source_tubes, dest_tubes_reverse, dest_tubes_normal):
+    if is_increase:
+        mix_repetitions = 8
+        pipette.transfer(transfer_vol, source(iteration, source_tubes),
+                         destination(is_increase, dest_tubes_reverse, dest_tubes_normal, iteration).top(-20),
+                         new_tip='never', blow_out=True,
+                         mix_before=(mix_repetitions, 200))
+    else:
+        pipette.transfer(transfer_vol, source(iteration, source_tubes),
+                         destination(is_increase, dest_tubes_reverse, dest_tubes_normal, iteration).top(-20),
+                         new_tip='never', blow_out=True)
+    #     mix_repetitions = 0
+    # pipette.transfer(transfer_vol, source(iteration, source_tubes),
+    #                  destination(is_increase, dest_tubes_reverse, dest_tubes_normal, iteration).top(-20),
+    #                  new_tip='never', blow_out=True,
+    #                  mix_before=(mix_repetitions, 200))
+    blow_outs(2, pipette)
+
+
+def blow_outs(times, pipette):
+    for i in range(times):
+        pipette.blow_out()
+
+
+# ********************************************************************************* #
+# invoke method
+aliquot_dil()
