cb55c8f8eef2bbbcd777767aa905b12f5054fa31
Ewa Sroczyk <es@nutopi.com>
2019-08-19 12:09:02 +0200

    A new custom rack for 6 big falcons instead of the old one (for 6 small and 4 big falcons) used. The rack is for two 50 ml falcons with two plasmas.

--- dilutions/SerialDilutionPlasma.py
+++ dilutions/SerialDilutionPlasma.py
@@ -7,21 +7,29 @@ counter_pipette1000 = 0
 
 def serial_dil(begin_vol, final_vol, tube_amount):
     # creates custom labware if currently not created
-    tube_2ml_rack_35 = "tube_2ml_rack_35"
-    if tube_2ml_rack_35 not in labware.list():
-        tube_2ml_rack_35 = labware.create("tube_2ml_rack_35",
-                                          grid=(7, 5),
-                                          spacing=(16.25, 14.56),
-                                          diameter=10,
-                                          depth=30)
+    tube_rack_35 = "tube_rack_35"
+    if tube_rack_35 not in labware.list():
+        tube_rack_35 = labware.create("tube_rack_35",
+                                      grid=(7, 5),
+                                      spacing=(16.25, 14.56),
+                                      diameter=10,
+                                      depth=41)
 
-    tubes1 = labware.load('tube_2ml_rack_35', slot='1')
-    tubes2 = labware.load('tube_2ml_rack_35', slot='2')
-    tubes3 = labware.load('tube_2ml_rack_35', slot='3')
-    tubes4 = labware.load('tube_2ml_rack_35', slot='4')
+    big_falcons_rack = "big_falcons_rack"
+    if big_falcons_rack not in labware.list():
+        big_falcons_rack = labware.create("big_falcons_rack",
+                                          grid=(3, 2),
+                                          spacing=(43, 45),
+                                          diameter=30,
+                                          depth=76.81)
+
+    tubes1 = labware.load('tube_rack_35', slot='1')
+    tubes2 = labware.load('tube_rack_35', slot='2')
+    tubes3 = labware.load('tube_rack_35', slot='3')
+    tubes4 = labware.load('tube_rack_35', slot='4')
     tip_rack_small = labware.load('opentrons-tiprack-300ul', slot='6')
     tip_rack_big = labware.load('tiprack-1000ul', slot='9')
-    plasma_falcon = labware.load('opentrons-tuberack-15_50ml', slot='5')
+    plasma_falcon = labware.load('big_falcons_rack', slot='5')
 
     tubes = [tubes1, tubes2, tubes3, tubes4]
 
@@ -43,7 +51,7 @@ def serial_dil(begin_vol, final_vol, tube_amount):
         return
 
     # dispensing plasma A from final volume to 0 ul, tubes iterated from the first to the last one
-    dispense_plasma(True, tube_amount, diff, final_vol, single50, single1000, plasma_falcon('A3'), tubes, current_vol)
+    dispense_plasma(True, tube_amount, diff, final_vol, single50, single1000, plasma_falcon('A1'), tubes, current_vol)
 
     current_vol = begin_vol
 
@@ -55,7 +63,7 @@ def serial_dil(begin_vol, final_vol, tube_amount):
     current_rack = 0
 
     # dispensing plasma B from final volume to 0 ul, tubes iterated from the last one to the first
-    dispense_plasma(False, tube_amount, diff, final_vol, single50, single1000, plasma_falcon('A4'), tubes, current_vol)
+    dispense_plasma(False, tube_amount, diff, final_vol, single50, single1000, plasma_falcon('A3'), tubes, current_vol)
 
     print(tube_amount, 'dilutions prepared.')
 
