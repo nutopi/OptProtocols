c1daf2f4c3db44d089d52064c70e23cd8ca27421
Ewa Sroczyk <es@nutopi.com>
2019-09-10 11:04:50 +0200

    New labware for cobas tubes added. Blow outs removed so as not to cause air bubbles (cobas does not like air bubbles).

--- dilutions/TransferToCobasTubes.py
+++ dilutions/TransferToCobasTubes.py
@@ -5,16 +5,25 @@ tube_rack_35 = "tube_rack_35"
 if tube_rack_35 not in labware.list():
     tube_rack_35 = labware.create("tube_rack_35",
                                   grid=(7, 5),
+                                  grid=(7, 5),
                                   spacing=(16.25, 14.56),
                                   diameter=10,
                                   depth=41)
 
-dest_tube_rack_A = labware.load('tube_rack_35', slot='4')
-dest_tube_rack_B = labware.load('tube_rack_35', slot='5')
-dest_tube_rack_C = labware.load('tube_rack_35', slot='6')
-dest_tube_rack_D = labware.load('tube_rack_35', slot='1')
-dest_tube_rack_E = labware.load('tube_rack_35', slot='2')
-dest_tube_rack_F = labware.load('tube_rack_35', slot='3')
+tube_rack_35 = "cobas_tubes_rack_35"
+if tube_rack_35 not in labware.list():
+    tube_rack_35 = labware.create("cobas_tubes_rack_35",
+                                  grid=(7, 5),
+                                  spacing=(16.25, 14.56),
+                                  diameter=9,
+                                  depth=35)
+
+dest_tube_rack_A = labware.load('cobas_tubes_rack_35', slot='4')
+dest_tube_rack_B = labware.load('cobas_tubes_rack_35', slot='5')
+dest_tube_rack_C = labware.load('cobas_tubes_rack_35', slot='6')
+dest_tube_rack_D = labware.load('cobas_tubes_rack_35', slot='1')
+dest_tube_rack_E = labware.load('cobas_tubes_rack_35', slot='2')
+dest_tube_rack_F = labware.load('cobas_tubes_rack_35', slot='3')
 source_tube_rack_A = labware.load('tube_rack_35', slot='7')
 source_tube_rack_B = labware.load('tube_rack_35', slot='8')
 source_tube_rack_C = labware.load('tube_rack_35', slot='9')
@@ -85,15 +94,7 @@ def liquid_transfer(used_pipette, transfer_vol, source, destination):
             used_pipette.aspirate(transfer_vol / 3, source.bottom(5))
             used_pipette.dispense(used_pipette.current_volume, destination)
 
-    blow_outs(2, used_pipette)
     used_pipette.drop_tip()
-
-
-# how much blow_outs is supposed to be performed
-def blow_outs(times, pipette):
-    for i in range(times):
-        pipette.blow_out()
-
 
 # ****************************************************************************************#
 
